69f8413038371c5b8a959877f885451998247d31
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-02-07 21:53:26 -0500

    Add files via upload

--- nyxapp/Handler/Env.js
+++ nyxapp/Handler/Env.js
@@ -1,9 +1,17 @@
+var pjson = require('../package.json');
+
 function cls(max)
 {
     console.clear();
 }
 
+function getVersion()
+{
+    console.log(pjson.name + " version = " + pjson.version);
+}
+
 module.exports =
 {
-    randomGen
+    cls,
+    getVersion
 }

675ba9b57b03939e5448e9d3dbffe8d471f9716a
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-04-08 23:32:04 -0400

    Remove unneeded lines from popup handler

--- nyxapp/Handler/Popup.js
+++ nyxapp/Handler/Popup.js
@@ -2,8 +2,6 @@ const { app, BrowserWindow, Notification } = require('electron');
 
 function show(title, body) 
 {
-    NOTIFICATION_TITLE = title;
-    NOTIFICATION_body = body;
     new Notification({ title: title, body: body }).show()
 }
 

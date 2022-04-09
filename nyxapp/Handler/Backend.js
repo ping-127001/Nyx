be4a7cd70ca7edf9f0daffe289ce1bbe7f3b4d83
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-04-09 10:41:56 -0400

    Add try catch

--- nyxapp/Handler/Backend.js
+++ nyxapp/Handler/Backend.js
@@ -1,25 +1,28 @@
 const request = require('request');
 
+const Alert = require("../Handler/Alert.js");
+
 let url = "https://raw.githubusercontent.com/ping-127001/Nyx/main/nyxBackend/backend.json";
 
 let options = {json: true};
 
-
 function RequestData()
 {
-    request(url, options, (error, res, json) => 
+    try
     {
-        if (error) 
+        request(url, options, (error, res, json) => 
         {
-            return  console.log("An error occured. Error: " + error)
-        };
-    
-        if (!error && res.statusCode == 200) 
-        {
-            //read data using "json" + json value name
-            console.log("Kill switch state: " + json.killswitch + " Version: " + json.version + " Debugging: " + json.debugging);
-        };
-    });
+            if (res.statusCode == 200) 
+            {
+                //read data using "json" + json value name
+                console.log("Kill switch state: " + json.killswitch + " Version: " + json.version + " Debugging: " + json.debugging);
+            };
+        });
+    }
+    catch (ex)
+    {
+        Alert.show("Error", ex);
+    }
 }
 
 module.exports =

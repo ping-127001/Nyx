9fc74523a979a105948776596aba101d71bf7579
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-04-09 19:57:48 -0400

    Add Discord presence handler

---
+++ nyxapp/Handler/Discord.js
@@ -0,0 +1,67 @@
+const client = require("discord-rich-presence")("962483669119930370");
+
+
+const exec = require('child_process').exec;
+
+const electron = require('electron');
+const dialog = electron.dialog;
+
+
+
+const isRunning = (query, cb) => 
+{
+    let platform = process.platform;
+    let cmd = '';
+    switch (platform) 
+    {
+        case 'win32' : cmd = `tasklist`; break;
+        case 'darwin' : cmd = `ps -ax | grep ${query}`; break;
+        case 'linux' : cmd = `ps -A`; break;
+        default: break;
+    }
+    exec(cmd, (err, stdout, stderr) => 
+    {
+        cb(stdout.toLowerCase().indexOf(query.toLowerCase()) > -1);
+    });
+}
+
+function bypassError()
+{
+    dialog.showErrorBox = function(title, content)  // disable error dialog by overriding, for some reason Electron doesn't like my function even though its fine.
+    {
+        console.log(`${title}\n${content}`);
+        console.clear();
+    };
+    //after further testing, i could not get it to give me an error anymore, but imma leave this here anyways.
+}
+
+function startDiscord()
+{
+    isRunning('discord.exe', (status) => 
+    {
+        console.log("Status: " + status)
+        if (status)
+        {
+            client.updatePresence
+            ({
+                state: 'Make friends that last forever!',
+                details: 'NyxApp',
+                startTimestamp: Date.now(),
+                largeImageKey: 'nyx',
+                largeImageText: "https://github.com/ping-127001/Nyx",
+                instance: true,
+              });
+        }
+        if (!status)
+        {
+            //client.disconnect();
+        }
+    })
+}
+
+
+module.exports =
+{
+    isRunning,
+    startDiscord
+}

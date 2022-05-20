49ef0bc446679393dcef07038cbb0b98db86b6c1
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-05-19 20:52:43 -0400

    Update main. Alot of debug code is in there

--- nyxapp/main.js
+++ nyxapp/main.js
@@ -1,8 +1,10 @@
 const { app, BrowserWindow} = require('electron');
 
-const Backend = require("./Handler/Backend.js");
+const isOnline = require('is-online');
 
-const Env = require("./Handler/Env.js");
+const Data = require("./Data/AppData.json");
+
+const ClientDefiner = require('./Handler/ClientDefiner.js');
 
 const Popup = require("./Handler/Popup.js");
 
@@ -12,11 +14,19 @@ const Discord = require('./Handler/Discord.js');
 
 const fstream = require('./Handler/fstream.js');
 
-const Websocket = require('./Handler/Websocket.js');
+const Socket = require("./Handler/Socket.js");
 
-const link = "http://127.0.0.1:8080/";
+const Webhook = require("./Handler/Webhook.js");
 
-var debugging = true
+var link = "http://127.0.0.1:8080/";
+
+var io = require("socket.io-client");
+
+var socket = io.connect("http://127.0.0.1:8080/");
+
+var Online;
+
+var debugging = false
 
 if (debugging)
 {
@@ -24,13 +34,30 @@ if (debugging)
   //this gets it to stop logging about not supporting GL or something stupid like that
 }
 
+//checkInternet();
+ClientDefiner.defineClientString(32);
+ClientDefiner.defineClientIp();
+
 app.whenReady().then(() =>
 {
+
     createWindow();
+    Socket.Send("new_msg", "Client connected");
     Discord.startDiscord();
+  
+  //if (Online)
+  //{
+    //createWindow();
+    //Socket.Send("new_msg", "Client connected");
+   // Discord.startDiscord();
+  //}
+  //else if (!Online)
+ // {
+  //  offlineWindow();
+  //}
 })
 
-function createWindow ()
+function createWindow()
 {
     try
     {
@@ -39,16 +66,55 @@ function createWindow ()
         height: 600,
         autoHideMenuBar: true, //hide menu bar
         icon: __dirname + './Images/Nyx.ico',
-        webPreferences: 
+        webPreferences:
         {
           devTools: false
-        }
+        },
       })
-      //win.loadFile('./html/home.html'); //used for local files
       win.loadURL(link);
     }
     catch (ex)
     {
       Alert.show("Error", ex);
     }
+}
+
+function offlineWindow()
+{
+  try
+  {
+    const win = new BrowserWindow({
+      width: 1000,
+      height: 600,
+      autoHideMenuBar: true, //hide menu bar
+      icon: __dirname + './Images/Nyx.ico',
+      webPreferences:
+      {
+        devTools: false
+      },
+    })
+    win.loadFile('./html/offline.html');
+  }
+  catch (ex)
+  {
+    Alert.show("Error", ex);
+  }
+}
+
+function recieveWebHooks()
+{
+  io.on("connection", (socket) =>
+  {
+    socket.on("new_msg", (data) => 
+    {
+      console.log(data);
+      Webhook.error = data;
+   });
+  });
+}
+
+async function checkInternet()
+{
+  Online = await isOnline();
+  console.log(Online);
 }

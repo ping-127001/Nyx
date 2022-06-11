ab0941d2a58fae6296725be1e1dfbb4ee1857e3d
BADTEMPER <66000596+BADTEMPER666@users.noreply.github.com>
2022-06-11 14:01:19 -0400

    Add linux support to most functions

--- nyxapp/Handler/fstream.js
+++ nyxapp/Handler/fstream.js
@@ -1,8 +1,8 @@
 const { time } = require("console");
-const fs = require("fs");
-const cfg = require('home-config').load('.myapprc')
 
-var os = require('os')
+const fs = require("fs");
+
+var os = require('os');
 
 const roamingDir = `C:/Users/${os.userInfo().username}/AppData/Roaming/NyxData`;
 
@@ -10,115 +10,137 @@ var folderExists = false;
 
 var logging = false;
 
-function createFolder(dir) 
+function createFolder(dir)
 {
-    try 
+    try
     {
         if (!fs.existsSync(dir)) 
         {
-            fs.mkdirSync(dir,
+            fs.mkdirSync(dir, 
                 {
                     recursive: true
                 });
         }
     }
-    catch (ex) 
+    catch (ex)
     {
         console.log(`There was an error creating a folder in ${dir} \n ${ex}`);
     }
 }
 
-function createFile(dir, name, extension, data) {
-    try {
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir,
+function createFile(dir, name, extension, data)
+{
+    try
+    {
+        if (!fs.existsSync(dir))
+        {
+            fs.mkdirSync(dir, 
                 {
                     recursive: true
                 });
 
             fs.writeFileSync(`${dir}/${name}.${extension}`, data);
         }
-        else if (fs.existsSync(dir)) {
+        else if (fs.existsSync(dir))
+        {
             fs.writeFileSync(`${dir}/${name}.${extension}`, data);
         }
     }
-    catch (ex) 
+    catch (ex)
     {
         console.log(`There was an error creating ${name}.${extension} in ${dir} \n ${ex}`);
     }
 }
 
-function createConfigFile(name, extension, data) {
-    try 
+function createConfigFile(name, extension, data)
+{
+    try
     {
         if (process.platform === "linux") 
         {
             const home = process.env.HOME;
-            var linuxRoaming = `${home}/.NyxApp`;
+            var linuxRoaming = `${home}/.NyxData`;
             fs.mkdirSync(linuxRoaming,
                 {
                     recursive: true
                 });
-            fs.writeFileSync(`${linuxRoaming}/${name}${extension}`, data);
+            fs.writeFileSync(`${linuxRoaming}/${name}.${extension}`, data);
         }
 
-        if (process.platform === "win32") 
+        if (process.platform === "win32")
         {
-            if (!fs.existsSync(roamingDir)) 
+            if (!fs.existsSync(roamingDir))
             {
-                fs.mkdirSync(roamingDir,
+                fs.mkdirSync(roamingDir, 
                     {
                         recursive: true
                     });
-
-                fs.writeFileSync(`${roamingDir}/${name}${extension}`, data);
+        
+                fs.writeFileSync(`${roamingDir}/${name}.${extension}`, data);
             }
-            else if (fs.existsSync(roamingDir)) {
-                fs.writeFileSync(`${roamingDir}/${name}${extension}`, data);
+            else if (fs.existsSync(roamingDir))
+            {
+                fs.writeFileSync(`${roamingDir}/${name}.${extension}`, data);
             }
         }
     }
-    catch (ex) 
+    catch (ex)
     {
-        if (process.platform === "win32") 
-        {
-            console.log(`There was an error creating a config file ${name}${extension} in ${roamingDir} \n ${ex}`);
-        }
+        console.log(`There was an error creating a config file ${name}.${extension} in ${roamingDir} \n ${ex}`);
+    }
+}
+
+function logError(error)
+{
+    try
+    {
         if (process.platform === "linux") 
         {
-            console.log(`There was an error creating a config file ${name}${extension} in ${linuxRoaming} \n ${ex}`);
-        }
-    }
-    //console.log(process.env.HOME)
-}
-
-function logError(error) 
-{
-    try {
-        var date = new Date().toISOString().slice(0, 10);
-        if (logging = true) {
-            if (!fs.existsSync(roamingDir)) {
-                fs.mkdirSync(roamingDir,
-                    {
-                        recursive: true
-                    });
+            const home = process.env.HOME;
+            var linuxRoaming = `${home}/.NyxData`;
+            var date = new Date().toISOString().slice(0, 10);
+            if (logging = true)
+            {
+                if (!fs.existsSync(roamingDir))
+                {
+                    fs.mkdirSync(roamingDir, 
+                        {
+                            recursive: true
+                        });
+                }
+                fs.writeFileSync(`${linuxRoaming}/error.log`, `${date} ` + `Error: ${error}`);
             }
-            fs.writeFileSync(`${roamingDir}/error.log`, `${date} ` + `Error: ${error}`);
+        }
+
+        if (process.platform === "win32")
+        {
+            var date = new Date().toISOString().slice(0, 10);
+            if (logging = true)
+            {
+                if (!fs.existsSync(roamingDir))
+                {
+                    fs.mkdirSync(roamingDir, 
+                        {
+                            recursive: true
+                        });
+                }
+                fs.writeFileSync(`${roamingDir}/error.log`, `${date} ` + `Error: ${error}`);
+            }
         }
     }
-    catch (ex) 
+    catch (ex)
     {
 
     }
 }
 
-function checkFolderExists(dir) 
+function checkFolderExists(dir)
 {
-    if (fs.existsSync(dir)) 
+    if (fs.existsSync(dir))
     {
         folderExists = true;
     }
-    if (!fs.existsSync(dir)) 
+    if (!fs.existsSync(dir))
     {
         folderExists = false;
     }

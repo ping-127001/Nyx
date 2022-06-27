30a389f3715c22a22ab193ef328cb23f6d0ff74a
romance <66000596+romance999@users.noreply.github.com>
2022-06-27 15:31:05 -0400

    Update pluginLoader.js

--- nyxapp/Handler/pluginLoader.js
+++ nyxapp/Handler/pluginLoader.js
@@ -1,4 +1,4 @@
-function loadPlugin(fileName, data) //you can pass data into the plugin loader for plugins that use a paramter, if no paramter is needed you can just leave it empty
+function loadPlugin(pluginName, fileName, data) //you can pass data into the plugin loader for plugins that use a paramter, if no paramter is needed you can just leave it empty
 {
   try
   {
